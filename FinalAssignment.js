738b9504d9cd287b879c1ec0781edb1fdd174b57
primordialsoupe <89666347+primordialsoupe@users.noreply.github.com>
2021-12-16 20:37:17 -0600

    Add files via upload

---
+++ FinalAssignment.js
@@ -0,0 +1,13 @@
+﻿let indexValue = 1;
+function slideShow() {
+    setTimeout(slideShow, 5000);
+    let x;
+    let img = document.getElementsByClassName("imgGallery");
+    for (x = 0; x < img.length; x++) {
+        img[x].style.display = "none";
+    }
+    indexValue++;
+    if (indexValue > img.length) { indexValue = 1 }
+    img[indexValue - 1].style.display = "block";
+}
+slideShow();
